9b88bce5f52efdd5d5aae96602ac8a82a2c6881a
Dana Larose <ywg.dana@gmail.com>
2022-10-29 21:50:01 -0500

    Rule engine example for chap 18

--- lesson18.fsx
+++ lesson18.fsx
@@ -21,3 +21,31 @@ let biggestFile inputs =
         
 let name, size = biggestFile files
 Console.WriteLine($"%s{name} is the largest file at %d{size} bytes")
+
+// A type aliases!
+type Rule = string -> bool * string // function signature that takes in string
+                                    // and returns a bool and a string
+
+let rules : Rule list =
+    [ fun text -> (text.Split ' ').Length = 3, "Must be three words"
+      fun text -> (text.Length <= 30, "Max length is 30 characters")
+      fun text -> text
+                  |> Seq.filter Char.IsLetter
+                  |> Seq.forall Char.IsUpper, "All letters must be uppercase"
+      fun text -> text
+                  |> Seq.filter Char.IsDigit
+                  |> Seq.length = 0, "No digits allowed" ]
+                  
+let buildValidator (rules : Rule list) =
+    rules
+    |> List.reduce(fun firstRule secondRule ->
+                   fun word ->
+                       let passed, error = firstRule word
+                       if passed then
+                           let passed, error = secondRule word
+                           if passed then true, "" else false, error
+                       else false, error)
+
+let validate = buildValidator rules
+let word = "HELLO FROM1 F#"
+Console.WriteLine(validate word)
